81fc572a4e56b650fb538a808c39ffbfb2df0f1a
zoutigo <zoutigo@gmail.com>
2022-04-24 12:08:55 +0200

    update production env variables

--- backend/database/config.js
+++ backend/database/config.js
@@ -25,11 +25,11 @@ module.exports = {
     },
   },
   production: {
-    username: process.env.PROD_DB_USERNAME,
-    password: process.env.PROD_DB_PASSWORD,
-    database: process.env.PROD_DB_NAME,
-    host: process.env.PROD_DB_HOSTNAME,
-    port: process.env.PROD_DB_PORT,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOSTNAME,
+    port: process.env.POSTGRES_PORT,
     dialect: 'postgres',
     logging: false,
     dialectOptions: {
