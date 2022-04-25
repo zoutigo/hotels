12a7b9c56c559acc0b1c6042f22e3a69fad77fa9
zoutigo <zoutigo@gmail.com>
2022-04-25 16:10:51 +0200

    remove ssl

--- backend/database/config.js
+++ backend/database/config.js
@@ -39,10 +39,6 @@ module.exports = {
       acquire: 30000,
       idle: 10000,
     },
-    dialectOptions: {
-      ssl: {
-        require: true,
-      },
-    },
+    dialectOptions: {},
   },
 }
