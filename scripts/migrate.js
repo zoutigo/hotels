89ea6c4db1c79fd3aaac16bc07855431157947c2
zoutigo <zoutigo@gmail.com>
2022-04-25 16:33:02 +0200

    add url string

---
+++ scripts/migrate.js
@@ -0,0 +1,25 @@
+const { execute } = require('@getvim/execute')
+const env = process.env.NODE_ENV || 'development'
+// const config = require(__dirname + '/../config/config.js')[env]
+
+const config = require(`${__dirname}/../backend/database/config.js`)[env]
+
+// const sequelize = new Sequelize(
+//   config.database,
+//   config.username,
+//   config.password,
+//   config
+// )
+// operatorsAliases: 0,
+// pool: {
+//   max: 5,
+//   min: 0,
+//   acquire: 30000,
+//   idle: 10000,
+// },
+// const URL= `postgres://{{user}}:{{password-with-chars-like-#-&}}@{{aws-rds-url}}}/{{database}}',
+const URL = `postgres://${config.username}:${config.password}@127.0.0.1:5432/${config.database}`
+
+//   DATABASE_URL="postgresql://symfony:ChangeMe@127.0.0.1:5432/app?serverVersion=13&charset=utf8"
+
+execute(`npx sequelize-cli db:migrate --url ${URL} --debug`)
