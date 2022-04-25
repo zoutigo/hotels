05aab38317101b1acd17e1c6b8ded92d3213f54d
zoutigo <zoutigo@gmail.com>
2022-04-25 16:44:32 +0200

    include promise

--- scripts/migrate.js
+++ scripts/migrate.js
@@ -18,12 +18,17 @@ const config = require(`${__dirname}/../backend/database/config.js`)[env]
 //   idle: 10000,
 // },
 // const URL= `postgres://{{user}}:{{password-with-chars-like-#-&}}@{{aws-rds-url}}}/{{database}}',
-const URL = `postgres://${config.username}:${config.password}@127.0.0.1:5432/${config.database}`
 
 //   DATABASE_URL="postgresql://symfony:ChangeMe@127.0.0.1:5432/app?serverVersion=13&charset=utf8"
 
-try {
-  execute(`npx sequelize-cli db:migrate --url ${URL} --debug`)
-} catch (error) {
-  console.log('our error:-------------------', error)
+const migrate = async () => {
+  const URL = `postgres://${config.username}:${config.password}@127.0.0.1:5432/${config.database}`
+
+  try {
+    await execute(`npx sequelize-cli db:migrate --url ${URL} --debug`)
+  } catch (error) {
+    console.log('our error:-------------------', error)
+  }
 }
+
+migrate()
