b27a2b70b552d19f38249bb8fca1d929abc26d96
zoutigo <zoutigo@gmail.com>
2022-05-03 14:25:46 +0200

    test new deploy

--- backend/database/config.js
+++ backend/database/config.js
@@ -1,18 +1,6 @@
 require('dotenv').config()
 
 module.exports = {
-  // development: {
-  //   username: process.env.POSTGRES_USER,
-  //   password: process.env.POSTGRES_PASSWORD,
-  //   database: process.env.POSTGRES_DB,
-  //   host: '127.0.0.1',
-  //   port: 5432,
-  //   dialect: 'postgres',
-  //   logging: true,
-  //   dialectOptions: {
-  //     bigNumberStrings: true,
-  //   },
-  // },
   development: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
