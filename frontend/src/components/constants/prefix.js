ef8c2a98ab85469e407b7e7f591ffa318b3bbdbc
zoutigo <zoutigo@gmail.com>
2022-05-04 08:31:59 +0200

    manually set server adress frontside

--- frontend/src/components/constants/prefix.js
+++ frontend/src/components/constants/prefix.js
@@ -1,4 +1,8 @@
+// export const IMG_PREFIX =
+//   process.env.NODE_ENV === 'production'
+//     ? process.env.SERVER_ADRESS
+//     : 'http://localhost:3500/images/'
 export const IMG_PREFIX =
   process.env.NODE_ENV === 'production'
-    ? process.env.SERVER_ADRESS
+    ? 'hotels.artsi.fr/'
     : 'http://localhost:3500/images/'
