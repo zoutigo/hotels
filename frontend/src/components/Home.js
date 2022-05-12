6909967e60fcce48588ef40273e9a1fe4c366aeb
zoutigo <zoutigo@gmail.com>
2022-05-12 13:32:27 +0200

    remove Home unused vars

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Grid } from '@mui/material'
+import { Box } from '@mui/material'
 import { styled } from '@mui/material/styles'
 
 import Header from './Header'
